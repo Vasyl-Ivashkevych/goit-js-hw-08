d47269b98f9d2947980726f33393b299411ce5b6
Vasilij_Ivashkevych <vasilijivashkevich@gmail.com>
2023-03-27 12:05:46 -0500

    Bag fixed

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,7 +8,6 @@ import { galleryItems } from './gallery-items';
 const galleryContainer = document.querySelector('.gallery');
 const itemsMarkup = createGalleryItemsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
-galleryContainer.addEventListener('click', onImgClick);
 
 function createGalleryItemsMarkup(items) {
   return items
@@ -33,33 +32,3 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionDelay: 250,
 });
 
-function onImgClick(e) {
-  e.preventDefault();
-
-  const isItemImage = e.target.classList.contains('gallery__image');
-  if (!isItemImage) return;
-
-  const currentImgUrl = e.target.dataset.source;
-
-  const instance = basicLightbox.create(
-    `
-		<img src="${currentImgUrl}" width="1280" height="auto"/>
-        `,
-    {
-      onShow: instance => {
-        window.addEventListener('keydown', onEscKeyPress);
-      },
-      onClose: instance => {
-        window.removeEventListener('keydown', onEscKeyPress);
-      },
-    }
-  );
-  instance.show();
-
-  function onEscKeyPress(e) {
-    const ESC_KEY_CODE = 'Escape';
-    const isEscKey = e.code === ESC_KEY_CODE;
-    if (!isEscKey) return;
-    instance.close();
-  }
-}
